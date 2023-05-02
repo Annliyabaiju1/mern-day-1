0bca80e62431770f8a947a6a98d2e0550ac8646e
Annliyabaiju1 <132336331+Annliyabaiju1@users.noreply.github.com>
2023-05-02 15:49:35 +0530

    my first project

---
+++ script.js
@@ -0,0 +1,119 @@
+// console.log("hello world");
+// var let const
+// var a=10
+// console.log(a);
+// var a=20
+// console.log(a);
+{
+// ?let b=4
+    // console.log(b);
+}
+// let b=34
+// console.log(b)
+/*let a=10;
+let b=20;
+console.log(a+b);
+console.log(a-b)
+console.log(a/b)
+console.log(a*b)
+console.log(b%a)
+
+let a=10
+let b="10"
+if(a===b)//double equal to checks value only while triple equalto checks datatype also
+{console.log(true)}
+else{
+    console.log(false)
+}
+let f="annliya  "
+let h="alse"
+let fn=f+h
+console.log( `my name is ${f} ${h}`);
+
+let car={
+    model:"cele",
+    color:"white",
+price:"100000"
+}
+console.log(car)
+ console.log(car.color)
+console.log(car.model)
+console.log(`my car is ${car.color} ${car.model}`)
+let person={
+    name:"ann",
+    college:"cce",
+    class:"cs",
+    address:{
+        housename:"pallippat",
+        street:"fry"
+    }
+}
+    console.log(`${person.name} ${person.address.housename}`)
+    let fruits=["apple","mango","orange"]
+    console.log(fruits.length)
+    fruits[3]="grapes";
+    fruits.push("pinneapple")
+    console.log(fruits)
+    fruits.unshift("dragon")
+    console.log(fruits)
+ fruits.pop(fruits)
+ fruits.shift(fruits)
+ let todo= [
+    {id:1,
+    name:"saya",
+},
+{id:2,
+name:"read"},
+{id:3,
+name:"yay"}
+ ]
+ console.log(todo)
+ 
+    
+    function addTwoNumbers(a,b)
+ { 
+    console.log(a+b);
+} 
+ function diffofnum(a,b)
+ {
+    console.log(a-b);
+ }
+ function multinum(a,b)
+ {
+    console.log(a*b);
+ }
+ (3,4)
+ diffofnum(19,4)
+ multinum(4,4)
+function addition(a,b){
+let s=a+b
+if(s>10)
+{
+    console.log("sum is greater than 10");
+}
+else{
+    console.log("sum is lesser than 10");
+}
+
+}
+addition(12,2);*/
+let numbers=[1,2,3,4,5,6,7,8,9];
+/*console.log(numbers);
+printing=(array) =>
+{
+    for(let i=0;i<array.length;i++)
+    {
+        console.log(array[i]);
+    }
+}
+printing(numbers);*/
+printing=(array) =>
+{
+    while(i<array.length)
+    {   
+        let i=0;
+        console.log(array[i]);
+        i++;
+    }
+}
+printing(numbers);
